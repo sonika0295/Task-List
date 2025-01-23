8ec22dce8f93fe9ca3033b59148ad5d1828408d5
sonika0295 <sonikakurmi48@gmail.com>
2025-01-23 17:46:27 +0530

    database setup

--- backend/database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ backend/database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,7 +12,9 @@ return new class extends Migration
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
+            $table->unsignedBigInteger('tokenable_id');
+            $table->string('tokenable_type', length: 100);
+            $table->index(['tokenable_id', 'tokenable_type']);
             $table->string('name');
             $table->string('token', 64)->unique();
             $table->text('abilities')->nullable();
