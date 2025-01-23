54b4b77090a66d96979acc3c53b567e9c7e96035
sonika0295 <sonikakurmi48@gmail.com>
2025-01-23 22:18:42 +0530

    api setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://13.55.161.110/backend/public',
         changeOrigin: true,
       }
     }
